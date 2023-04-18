c25b9ec7603ba1cf94818553ffc7504b4dfc69e1
Alexandre Sorza <44484412+sorza@users.noreply.github.com>
2023-04-18 20:22:20 -0300

    Add files via upload

---
+++ AULAS/PHP-Estruturado/POST_GET/code/desafio/adivinhe_senha.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Passagem de Parâmetros via GET</title>
+</head>
+<body>
+    <form action="gabarito_senha.php" method="POST">
+        <label for="senha">Digite a senha:</label>
+        <input type="password" id="senha" name="senha"><br><br>
+        <input type="text" id="nome" name="nome"><br><br>
+        <input type="date" id="data" name="data"><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
